ccb44574b8ab14126dbee7aef7195dcb76ed4d16
Ethen Kuether <ekuether88@gmail.com>
2022-11-02 17:35:03 -0500

    Added DB tables into the init.sql

--- db/docker_postgres_init.sql
+++ db/docker_postgres_init.sql
@@ -1,12 +1,41 @@
 CREATE USER me;
-CREATE DATABASE api;
-GRANT ALL PRIVILEGES ON DATABASE api TO me;
-\c api 
-CREATE TABLE users (
+CREATE DATABASE dungeon IF NOT EXISTS;
+GRANT ALL PRIVILEGES ON DATABASE dungeon TO me;
+\c dungeon
+CREATE TABLE gamelevel IF NOT EXISTS (
   ID SERIAL PRIMARY KEY,
   name VARCHAR(30),
-  email VARCHAR(30)
+  levelstart INT
 );
 
-INSERT INTO users (name, email)
-  VALUES ('Jerry', 'jerry@example.com'), ('George', 'george@example.com');
+CREATE TABLE obstacle IF NOT EXISTS (
+  ID SERIAL PRIMARY KEY,
+  weakness INT,
+  room INT,
+  name VARCHAR(30)
+);
+
+CREATE TABLE item IF NOT EXISTS (
+  ID SERIAL PRIMARY KEY,
+  possessed INT,
+  room INT,
+  name VARCHAR(30)
+);
+
+CREATE TABLE player IF NOT EXISTS (
+  ID SERIAL PRIMARY KEY,
+  user VARCHAR(255),
+  room INT,
+  maxitems INT,
+  numitems INT,
+  name VARCHAR(255)
+)
+
+CREATE TABLE room IF NOT EXISTS (
+  ID SERIAL PRIMARY KEY,
+  above INT,
+  below INT,
+  left INT,
+  right INT
+)
+
